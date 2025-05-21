90c517f27dea9cab0b2903748d96b15b5da57133
suragch <suragch@suragch.dev>
2025-05-21 09:25:38 +0800

    fix errors with verse population

--- supplemental/database_creator/lib/create_ot_database.dart
+++ supplemental/database_creator/lib/create_ot_database.dart
@@ -4,27 +4,26 @@ import 'package:database_creator/database_helper.dart';
 
 Future<void> createOtDatabase() async {
   final dbHelper = DatabaseHelper();
-  dbHelper.init();
   dbHelper.deleteDatabase();
-  await _populateVersesTable();
+  dbHelper.init();
+  await _populateVersesTable(dbHelper);
   await _populateStrongsTable();
 }
 
-Future<void> _populateVersesTable() async {
-  final hebrewPath = '../hebrew/interlinear.csv';
-  final hebrewLines = await File(hebrewPath).readAsLines();
-  final interlinear = _makeInterlinearMap(hebrewLines);
-  final dbHelper = DatabaseHelper();
-
+Future<void> _populateVersesTable(DatabaseHelper dbHelper) async {
+  final interlinear = await _makeInterlinearMap();
   for (int i = 1; i <= 39; i++) {
     print('Populating book $i');
     await _populateVersesForBook(dbHelper, i, interlinear);
   }
 }
 
-Map<String, String> _makeInterlinearMap(List<String> lines) {
+Future<Map<String, String>> _makeInterlinearMap() async {
+  final hebrewPath = '../hebrew/interlinear.csv';
+  final lines = await File(hebrewPath).readAsLines();
   final map = <String, String>{};
-  for (int i = 0; i < lines.length; i++) {
+  // skip csv header so start at line 1
+  for (int i = 1; i < lines.length; i++) {
     final line = lines[i];
     final parts = line.split('\t');
     final key = '${parts[0]}-${parts[1]}-${parts[2]}';
@@ -36,21 +35,22 @@ Map<String, String> _makeInterlinearMap(List<String> lines) {
 Future<void> _populateVersesForBook(DatabaseHelper dbHelper, int bookId, Map<String, String> interlinear) async {
   final inputFile = File('../original_docs/csv/$bookId.csv');
   final lines = await inputFile.readAsLines();
-  for (int i = 0; i < lines.length; i++) {
+  // skip csv header so start at line 1
+  for (int i = 1; i < lines.length; i++) {
     final line = lines[i];
     final parts = line.split('\t');
-    final kjvChapter = int.tryParse(parts[3]);
-    final kjvVerse = int.tryParse(parts[4]);
+    final kjvChapter = int.tryParse(parts[4]);
+    final kjvVerse = int.tryParse(parts[5]);
     final key = '$bookId-$kjvChapter-$kjvVerse';
     final hebrewVerse = interlinear[key];
     dbHelper.insertVerseLine(
       bookId: bookId,
-      ivChapter: int.tryParse(parts[0]),
-      ivVerse: int.tryParse(parts[1]),
-      ivText: parts[2] == 'null' ? null : parts[2],
+      ivChapter: int.tryParse(parts[1]),
+      ivVerse: int.tryParse(parts[2]),
+      ivText: parts[3] == 'null' ? null : parts[3],
       kjvChapter: kjvChapter,
       kjvVerse: kjvVerse,
-      kjvText: parts[5] == 'null' ? null : parts[5],
+      kjvText: parts[5] == 'null' ? null : parts[6],
       originalText: hebrewVerse,
     );
   }
